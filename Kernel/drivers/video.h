5230a103660b3baa67236f0600c72cda6178158a
Facundo López Llopis <flopezllopis@itba.edu.ar>
2025-10-08 21:57:13 +0000

    VESA Config

---
+++ Kernel/drivers/video.h
@@ -0,0 +1,20 @@
+#ifndef GRAPHICS_H
+#define GRAPHICS_H
+#include <stdint.h>
+
+void canvas_mode();
+void clear_canvas();
+void draw_pixel(uint64_t x, uint64_t y, uint32_t color);
+void draw_line(uint64_t x1, uint64_t y1, uint64_t x2, uint64_t y2, uint32_t color);
+void draw_rectangle(uint64_t x, uint64_t y, uint16_t width, uint16_t height, uint32_t color);
+void draw_circle(uint64_t x, uint64_t y, uint16_t radius, uint32_t color);
+void draw_text(uint64_t x, uint64_t y, const char* text, uint32_t color);
+
+void text_mode();
+void clear_screen();
+void print(const char* text);
+void printChar(char c);
+void deleteChar();
+void newLine();
+
+#endif
--- Userland/Shell/commands/help.c
+++ Userland/Shell/commands/help.c
@@ -1,6 +1,6 @@
 #include <commands.h>
 
-int help() {
+int help(char ** argv, int argc) {
     println("Lista de comandos:\n help - Obtiene la lista de comandos\n");
     return 0;
 }
