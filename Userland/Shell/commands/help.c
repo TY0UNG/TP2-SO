aee545c1693b68b046866c29f6abb7f1f564a426
jesus  Gabriel Bastidas Diaz <gabobas124@gmail.com>
2025-10-23 12:53:36 -0300

    agrego comandos a help

--- Userland/Shell/commands/help.c
+++ Userland/Shell/commands/help.c
@@ -1,6 +1,61 @@
 #include <commands.h>
 
+typedef struct {
+    char *name;
+    char *description;
+    char *category;
+} Commands;
+
+Commands command_list[] = {
+    // SISTEMA
+    {"help", " display online manual documentation pages", "  SISTEM"},
+    {"shutdown", " close down the system at a given time", "  SISTEM"},
+    // OTRAS FUNC
+    {"time", " Muestra fecha y hora actual", "  DATA SIST."},
+    {"regs", " Muestra registros del CPU", "  DATA SIST."},
+    //BENCHMARKING
+    {"fps", " Muestra frames por segundo", "  BENCHMARKING"},
+    // UTILIDADES
+    {"echo", " write arguments to the standard output", "  UTILITIES"},
+    // JUEGO
+    {"tron", " Juego Tron", "  GAME"},
+    {"bounce", " Animacion rebotando", "  GAME"},
+    {NULL, NULL, NULL}
+};
+
+void print_command(const char *name, const char *description) {
+    int maxNameWidth = 12; // columna donde empieza el "-"
+    int len = 0;
+    while (name[len] != '\0') len++;
+
+    print("     "); print(name);
+    
+    for (int i = len; i < maxNameWidth; i++)
+        printChar(' '); 
+        
+    print(" - ");  println(description);
+
+}
+
+
+
 int help(char ** argv, int argc) {
-    println("Lista de comandos:\n help - Obtiene la lista de comandos\n");
+    println("  Lista de comandos:\n help - Obtiene la lista de comandos\n");
+
+    println("========= COMANDOS DISPONIBLES ================================");
+    
+    char *current = NULL;
+    
+    for (int i = 0; command_list[i].name != NULL; i++) {
+        // Si cambia de categ
+        if (current == NULL || strcmp(current, command_list[i].category) != 0) {
+            current = command_list[i].category;
+            printChar('\n');  println(current);
+        }
+        
+         print_command(command_list[i].name, command_list[i].description);
+    }
+    println("\n===============================================================");
+
     return 0;
 }
